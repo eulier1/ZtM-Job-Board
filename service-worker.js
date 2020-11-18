952046bab6ce82bea18c701d203d9f58ae48cb33
MattCSmith <github@matt-smith.dev>
2020-11-18 13:56:41 +0000

    Deploying to gh-pages from  @ d8c0b3ff0faaaa0978838c3c68dcd3dcdd9917b3 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f4ea68dca60ada3677f2b287383742a0.js"
+  "./precache-manifest.5340fde53d3007136ce1215467bf0527.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
